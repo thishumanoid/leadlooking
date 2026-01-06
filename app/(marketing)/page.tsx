d1dea675a8e74480cc15bda30df850f0845cc0e2
himan <logicallhimanshu@gmail.com>
2026-01-06 15:16:24 +0530

    removed the import of pricing cards 2

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,7 +3,6 @@ import Hero from '@/components/hero';
 import Steps from '@/components/steps';
 import Features from '@/components/features';
 import PricingCard from '@/components/pricingCards';
-import PricingCards2 from '@/components/pricingCards2';
 import FAQ from '@/components/faq';
 import CTA from '@/components/CTA';
 import { Testimonials } from '@/components/testimonials';
