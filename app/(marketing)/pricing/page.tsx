0475d4053dbc5b07d88f0485abd9460dcd1d199a
himan <logicallhimanshu@gmail.com>
2026-02-28 21:34:36 +0530

    remove clerk auth

--- app/(marketing)/pricing/page.tsx
+++ app/(marketing)/pricing/page.tsx
@@ -1,16 +1,19 @@
-
+'use client'
 import type { Metadata } from 'next';
 import AnimationContainer from '@/components/global/animation-container';
 import MaxWidthWrapper from '@/components/global/MaxWidthWrapper';
 import PricingCards from '@/components/pricingCards';
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react';
 
-export const metadata: Metadata = {
-  alternates: {
-    canonical: 'https://www.leadlooking.com/',
-  }
-};
 
 const PricingPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('https://tagfast.web.app/pricing');
+  }, []);
+
   return (
     <MaxWidthWrapper className="mb-40">
       <AnimationContainer delay={0.2}>
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -22,12 +22,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
     {
-      url: `${baseUrl}/pricing`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
       url: `${baseUrl}/privacy`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
